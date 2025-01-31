00c4b76c6daf8fd374f62dd3b28c096141421a71
Tim <tim-gideon.quell@stud.hshl.de>
2025-01-31 22:28:04 +0100

    updated ui for android

--- composeApp/src/androidMain/kotlin/de/hshl/isd/companion/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/de/hshl/isd/companion/MainActivity.kt
@@ -4,15 +4,41 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import de.hshl.isd.companion.ui.CompanionTheme
+import de.hshl.isd.companion.ui.theme.ThemeManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            App()
+            CompanionTheme(darkTheme = ThemeManager.isDarkMode) {
+                SetStatusBarAndNavigationBarColor(window, ThemeManager.isDarkMode)
+                App()
+            }
+        }
+    }
+}
+
+@Composable
+fun SetStatusBarAndNavigationBarColor(window: android.view.Window, darkTheme: Boolean) {
+    val backgroundColor = MaterialTheme.colorScheme.background.toArgb()
+    SideEffect {
+        window.statusBarColor = backgroundColor
+        window.navigationBarColor = backgroundColor
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        if (darkTheme) {
+            windowInsetsController.isAppearanceLightStatusBars = false
+            windowInsetsController.isAppearanceLightNavigationBars = false
+        } else {
+            windowInsetsController.isAppearanceLightStatusBars = true
+            windowInsetsController.isAppearanceLightNavigationBars = true
         }
     }
 }
